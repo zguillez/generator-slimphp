f7086df0badb88073c0f773a8cecbd2d393c6b97
Guillermo de la Iglesia <zguillez@gmail.com>
2016-12-21 20:18:06 +0100

    fix virtual host task

--- generators/app/templates/grunt/open.js
+++ generators/app/templates/grunt/open.js
@@ -1,12 +1,19 @@
 'use strict';
-module.exports = function (grunt) {
-	var config = grunt.file.readJSON('config.json');
-	grunt.config.set('open', {
-		dev: {
-			path: config.dev_domain + '/' + config.www_folder
-		},
-		pro: {
-			path: config.pro_domain + '/' + config.www_folder
-		}
-	});
+module.exports = function( grunt ) {
+  var config = grunt.file.readJSON( 'config.json' );
+  console.log( '' );
+  console.log( '**********************************************************************' );
+  console.log( 'Dev sever: ' + config.dev_domain + '/' + config.www_folder );
+  console.log( 'Enable virtual host with: sudo nano -w /etc/hosts' );
+  console.log( 'Path: ' + config.dev_path );
+  console.log( '**********************************************************************' );
+  console.log( '' );
+  grunt.config.set( 'open', {
+    dev: {
+      path: config.dev_domain + '/' + config.www_folder
+    },
+    pro: {
+      path: config.pro_domain + '/' + config.www_folder
+    }
+  } );
 };
