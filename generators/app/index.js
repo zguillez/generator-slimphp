6fa69d9f94b23dc5ca6a61efc71dcb6dc17f45c0
Guillermo de la Iglesia <zguillez@gmail.com>
2017-10-26 00:32:24 +0200

    bin folder

--- generators/app/index.js
+++ generators/app/index.js
@@ -38,6 +38,7 @@ module.exports = class extends Generator {
     this.fs.copy(this.templatePath('composer.lock'), this.destinationPath('composer.lock'));
     this.fs.copy(this.templatePath('composer.phar'), this.destinationPath('composer.phar'));
     this.fs.copy(this.templatePath('index.php'), this.destinationPath('index.php'));
+    this.fs.copy(this.templatePath('bin'), this.destinationPath('bin'));
     this.fs.copy(this.templatePath('inc'), this.destinationPath('inc'));
     this.fs.copy(this.templatePath('logs'), this.destinationPath('logs'));
     this.fs.copy(this.templatePath('htaccess'), this.destinationPath('.htaccess'));
