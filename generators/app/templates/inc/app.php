ebf1a277e068e593ab0340ec640f826cbe10df56
Guillermo de la Iglesia <zguillez@gmail.com>
2017-12-05 17:05:27 +0100

    Update app.php

--- generators/app/templates/inc/app.php
+++ generators/app/templates/inc/app.php
@@ -38,18 +38,18 @@
     {
       $this->app->run();
     }
-    public function route($myroute, $method, $callback)
+    public function route($route, $method, $callback)
     {
       if($method === 'POST') {
-        $this->app->post($myroute, $callback);
+        $this->app->post($route, $callback);
       } else if($method === 'GET') {
-        $this->app->get($myroute, $callback);
+        $this->app->get($route, $callback);
       } else if($method === 'PUT') {
-        $this->app->put($myroute, $callback);
+        $this->app->put($route, $callback);
       } else if($method === 'DELETE') {
-        $this->app->delete($myroute, $callback);
+        $this->app->delete($route, $callback);
       }
-      array_push($this->routes, $myroute);
+      array_push($this->routes, $route);
     }
     public function response($response, $data = '', $status = 200, $type = 'text/html')
     {
